9ba96b299877e011cc13e629a0f5b491223d138c
szkrd <szabolcs.kurdi@gmail.com>
2018-01-18 11:30:22 +0100

    using elementrefs

--- src/app/table-of-contents/table-of-contents.component.ts
+++ src/app/table-of-contents/table-of-contents.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterContentInit, Component, ElementRef, OnInit} from '@angular/core';
 import {Link} from './link';
 
 @Component({
@@ -6,7 +6,9 @@ import {Link} from './link';
   templateUrl: './table-of-contents.component.html',
   styleUrls: ['./table-of-contents.component.css']
 })
-export class TableOfContentsComponent implements OnInit {
+export class TableOfContentsComponent implements OnInit, AfterContentInit {
+  el: HTMLElement;
+
   links: Link[] = [
     { url: 'https://angular-2-training-book.rangle.io/', title: 'a2 training book' },
     { url: 'https://angular.io/tutorial', title: 'Tour of Heroes' },
@@ -14,9 +16,18 @@ export class TableOfContentsComponent implements OnInit {
     { url: 'https://blog.angular.io/', title: 'Angular blog' }
   ];
 
-  constructor() { }
+  constructor(
+    private elementRef: ElementRef
+  ) {
+    this.el = elementRef.nativeElement;
+  }
 
   ngOnInit() {
+  }
+
+  // this is how you access and modify the dom (but you would do that in a directive I hope)
+  ngAfterContentInit () {
+    this.el.querySelector('pre.code').innerHTML = '<b>html injected</b>';
   }
 
 }
